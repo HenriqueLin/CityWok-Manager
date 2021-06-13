5579c52772614a4cb12097617cc6d204d6d47785
Henrique Lin <henrique.lin99@gmail.com>
2021-06-13 17:00:33 +0100

    Update drop command

--- citywok_ms/cli.py
+++ citywok_ms/cli.py
@@ -1,5 +1,7 @@
+import os
+import shutil
 import click
-from flask import Blueprint
+from flask import Blueprint, current_app
 from flask.cli import AppGroup
 from citywok_ms import db
 
@@ -19,9 +21,20 @@ def create():
 @db_cli.command("drop")
 @click.confirmation_option(prompt="Are you sure you want to drop the db?")
 def drop():
-    "Drop the database"
+    "Drop the database and remove all user files"
     db.drop_all()
     click.echo("Dropped all tables.")
+
+    path = current_app.config["UPLOAD_FOLDER"]
+    for filename in os.listdir(path):
+        file_path = os.path.join(path, filename)
+        if os.path.isfile(file_path) or os.path.islink(file_path):
+            os.unlink(file_path)
+        elif os.path.isdir(file_path):
+            shutil.rmtree(file_path)
+        click.echo(f"Deleted {filename}.")
+    click.echo("Deleted all files.")
+
 
 
 command.cli.add_command(db_cli)
