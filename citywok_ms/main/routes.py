1b082c5511fc6e2ced8669a5c926fdd3eb30a0dc
Henrique Lin <henrique.lin99@gmail.com>
2021-06-16 00:15:25 +0100

    Update main page permission

--- citywok_ms/main/routes.py
+++ citywok_ms/main/routes.py
@@ -1,8 +1,10 @@
 from flask import Blueprint, render_template
+from citywok_ms.auth.permissions import visitor
 
 main = Blueprint("main", __name__)
 
 
 @main.route("/home")
+@visitor.require(401)
 def index():
     return render_template("main/index.html", title="Home")
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,14 +1,13 @@
 from flask import url_for
 
 
-def test_index_get(client):
+def test_index_get(client, admin):
     response = client.get(url_for("main.index"))
     data = response.data.decode()
 
     # state code
     assert response.status_code == 200
 
-    print(data)
     assert "CityWok-Manager" in data
     assert "Home" in data
 
