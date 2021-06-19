5a73b0ca68ccc49918dd1ea9db96cdc094d819cb
Henrique Lin <henrique.lin99@gmail.com>
2021-06-19 16:49:02 +0100

    Add error handler for 404 and 500

--- citywok_ms/error/handlers.py
+++ citywok_ms/error/handlers.py
@@ -1,6 +1,7 @@
 from citywok_ms.auth.messages import REQUIRE_LOGIN
 from flask import Blueprint, render_template, redirect
 from flask.helpers import flash, url_for
+from citywok_ms import db
 
 error = Blueprint("error", __name__)
 
@@ -15,3 +16,13 @@ def unauthorized(error):
 def forbidden(error):
     return render_template("error/403.html"), 403
 
+
+@error.app_errorhandler(404)
+def not_found_error(error):
+    return render_template("error/404.html"), 404
+
+
+@error.app_errorhandler(500)
+def internal_error(error):
+    db.session.rollback()
+    return render_template("error/500.html"), 500
