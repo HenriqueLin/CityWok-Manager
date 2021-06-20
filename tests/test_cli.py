524c39ecfc04e63853e45961e42bd901e637124b
Henrique Lin <henrique.lin99@gmail.com>
2021-06-20 21:31:20 +0100

    Add tests for i18n click commands

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,18 +1,21 @@
-from flask.testing import FlaskCliRunner
+import os
+
 import pytest
-from citywok_ms.cli import create, drop, load_example
-from flask.cli import shell_command
 from citywok_ms import db
+from citywok_ms.cli import compile, create, drop, init, load_example, update
+from flask.cli import shell_command
+from flask.testing import FlaskCliRunner
+import shutil
 
 
-def test_db_load_without_create(app_without_db):
+def test_dev_load_without_create(app_without_db):
     runner = FlaskCliRunner(app_without_db)
     result = runner.invoke(load_example)
     assert not result.exception
     assert result.output == "Please create database first.\n"
 
 
-def test_db_load_duplicate(app_without_db):
+def test_dev_load_duplicate(app_without_db):
     runner = FlaskCliRunner(app_without_db)
     db.create_all()
     runner.invoke(load_example)
@@ -21,7 +24,7 @@ def test_db_load_duplicate(app_without_db):
     assert result.output == "Database already loaded.\n"
 
 
-def test_db_load(app_without_db):
+def test_dev_load(app_without_db):
     runner = FlaskCliRunner(app_without_db)
     db.create_all()
     result = runner.invoke(load_example)
@@ -29,7 +32,7 @@ def test_db_load(app_without_db):
     assert result.output == "Loaded example entities.\n"
 
 
-def test_db_create(app_without_db):
+def test_dev_create(app_without_db):
     runner = FlaskCliRunner(app_without_db)
     result = runner.invoke(create)
 
@@ -39,7 +42,7 @@ def test_db_create(app_without_db):
 
 
 @pytest.mark.parametrize("input, output", [("y", "Dropped"), ("n", "Abort")])
-def test_db_drop_yes(app_without_db, input, output):
+def test_dev_drop_yes(app_without_db, input, output):
     runner = FlaskCliRunner(app_without_db)
     db.create_all()
     runner.invoke(load_example)
@@ -54,3 +57,26 @@ def test_shell(app_without_db):
 
     assert result.exit_code == 0
     assert app_without_db.name in result.output
+
+
+def test_i18n(app_without_db):
+    path = os.path.join(app_without_db.root_path, "translations/es")
+    # i18n init
+    runner = FlaskCliRunner(app_without_db)
+    result = runner.invoke(init, "es -q")
+
+    assert not result.exception
+    assert os.path.isdir(path)
+    assert os.path.isfile(os.path.join(path, "LC_MESSAGES/messages.po"))
+
+    # i18n update
+    result = runner.invoke(update, "-q")
+    assert not result.exception
+
+    # i18n compile
+    result = runner.invoke(compile, "-q")
+    assert not result.exception
+    assert os.path.isfile(os.path.join(path, "LC_MESSAGES/messages.mo"))
+
+    # clean up
+    shutil.rmtree(path)
