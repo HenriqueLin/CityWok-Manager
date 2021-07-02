6ff32266b455b4345b87aa930dfb8c0f79d8f65c
Henrique Lin <henrique.lin99@gmail.com>
2021-07-02 21:31:22 +0100

    Add functionality to edit orders

--- citywok_ms/order/routes.py
+++ citywok_ms/order/routes.py
@@ -1,6 +1,6 @@
 from citywok_ms.task import compress_file
 from citywok_ms.order.models import Order
-from citywok_ms.order.forms import OrderForm
+from citywok_ms.order.forms import OrderForm, OrderUpdateForm
 from flask import Blueprint, flash, redirect, url_for, render_template, current_app
 from citywok_ms.auth.permissions import manager
 from citywok_ms import db
@@ -34,3 +34,30 @@ def new():
         current_app.logger.info(f"Create order {order}")
         return redirect(url_for("order.new"))  # FIXME:
     return render_template("order/new.html", title=_("New Order"), form=form)
+
+
+@order_bp.route("/<order_id>/update", methods=["GET", "POST"])
+@manager.require(403)
+def update(order_id):
+    order = Order.get_or_404(order_id)
+    form = OrderUpdateForm()
+    form.hide_id.data = order.id
+
+    if form.validate_on_submit():
+        order.update_by_form(form)
+        flash(
+            _("Order has been updated."),
+            "success",
+        )
+        db.session.commit()
+        current_app.logger.info(f"Update order {order}")
+        return redirect(url_for("order.new"))  # FIXME:
+
+    form.process(obj=order)
+
+    return render_template(
+        "order/update.html",
+        order=order,
+        form=form,
+        title=_("Update Order"),
+    )
